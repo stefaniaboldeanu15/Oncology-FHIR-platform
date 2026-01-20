8cea7b043985f7cda7da1628a4c8fc1f83f683e5
stefaniaboldeanu15 <stefaniaboldeanu15@gmail.com>
2026-01-21 00:45:03 +0200

    Initial commit

---
+++ src/main/java/org/app/oncologyfhir/OncologyfhirApplication.java
@@ -0,0 +1,13 @@
+package org.app.oncologyfhir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OncologyfhirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OncologyfhirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/app/oncologyfhir/OncologyfhirApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.oncologyfhir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OncologyfhirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
